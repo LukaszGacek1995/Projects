93ae77e2bf77219a1dd872cb98afb3589bb6fec3
Łukasz Gacek <lukasz.gacek.1995@gmail.com>
2023-12-18 19:38:42 +0100

    Utworzenie mechanizmu uruchamiania z jednego projektu

---
+++ Library/UruchamianieProgramu.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    public class UruchamianieProgramu
+    {
+        public static void KlasaWywolujaca()
+        {
+            ParametryWejsciowe parametry = new ParametryWejsciowe();
+            var listaParametrow = parametry.Wprowadzenie();
+
+            MechanizmSprawdzaniaPozycjiLogika mechanizmSprawdzaniaPozycjiLogika = new MechanizmSprawdzaniaPozycjiLogika();
+            var logika = mechanizmSprawdzaniaPozycjiLogika.GlownaLogika(listaParametrow);
+
+            foreach (var iteracjaWyniku in logika)
+            {
+                Console.WriteLine(iteracjaWyniku);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
